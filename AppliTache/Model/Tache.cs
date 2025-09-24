e28aac8054acbcc0d1300350cb8f926dd657dc19
Mezier <50947908+TheD3us@users.noreply.github.com>
2025-09-24 10:47:21 +0200

    ajout objet Tache

---
+++ AppliTache/Model/Tache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliTache.Model
+{
+    class Tache
+    {
+        private string titre;
+        private string description;
+        private string statut;   // à faire, attribué, fini
+        // private User user;
+
+        public Tache()
+        { }
+
+        public Tache(string Titre, string Description, string Statut)
+        {
+            this.titre = Titre;
+            this.description = Description;
+            this.statut = Statut;
+        }
+
+        public string Titre
+        { get; set; }
+
+        public string Description
+        { get; set; }
+
+        public string Statut
+        { get; set; }
+
+
+    }
+}
