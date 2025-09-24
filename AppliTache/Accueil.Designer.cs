ff5f37ca0cc20185b036b63cafee4a6e468b226c
Mezier <50947908+TheD3us@users.noreply.github.com>
2025-09-24 10:03:27 +0200

    Modif Form

--- AppliTache/Accueil.Designer.cs
+++ AppliTache/Accueil.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AppliTache
 {
-    partial class Form1
+    partial class Accueil
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BAjoutTache = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BAjoutTache
+            // 
+            this.BAjoutTache.Location = new System.Drawing.Point(21, 24);
+            this.BAjoutTache.Name = "BAjoutTache";
+            this.BAjoutTache.Size = new System.Drawing.Size(75, 23);
+            this.BAjoutTache.TabIndex = 0;
+            this.BAjoutTache.Text = "Ajout Tâche";
+            this.BAjoutTache.UseVisualStyleBackColor = true;
+            // 
+            // Accueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.BAjoutTache);
+            this.Name = "Accueil";
+            this.Text = "Accueil";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BAjoutTache;
     }
 }
 
