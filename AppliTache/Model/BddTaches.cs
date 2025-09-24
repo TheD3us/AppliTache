257c3528a7f55ddbbc47a0b647d87e45d948a55f
latexier <la.texier@gmail.com>
2025-09-24 12:35:00 +0200

    Ajoujt d'une fausse base de données de tache avec ajout et suppresion, et get

---
+++ AppliTache/Model/BddTaches.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliTache.Model
+{
+    class BddTaches
+    {
+        private List<Tache> liste;
+
+        public List<Tache> GetList()
+        {
+            return liste;
+        }
+
+        public void Add(Tache t)
+        {
+            liste.Add(t);
+        }
+
+        public void Delete(Tache t)
+        {
+            foreach(Tache x in liste)
+            {
+                if (x.Titre == t.Titre)
+                {
+                    liste.Remove(t);
+                }
+            }
+        }
+    }
+}
